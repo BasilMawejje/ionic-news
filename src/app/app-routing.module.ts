94c8b3c83eac1c7f03355523358ffa28aa510794
BasilMawejje <basil.mawejje@gmail.com>
2019-02-13 12:05:53 +0300

    Able to pull data in ionic 4 side menu template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'news-single', loadChildren: './news-single/news-single.module#NewsSinglePageModule' }
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  data : any;
+  constructor(private newsService: NewsService, private router: Router) { }
+
+  ngOnInit() {
+    this.newsService
+        .getData('everything?q=bitcoin&from=2019-01-13&sortBy=publishedAt')
+        .subscribe(data => {
+          console.log(data);
+          this.data = data;
+        });
+
+  }
+
+  onGoToNewsSinglePage(article){
+      this.newsService.currentArticle = article;
+      this.router.navigate(['/news-single']);
+  }
 
 }

72383f3f8cb2d458840cfd4d0b1774cb131390ba
BasilMawejje <basil.mawejje@gmail.com>
2019-02-22 16:41:22 +0300

    Able to share story to twitter and whatsapp

--- src/app/news-single/news-single.page.ts
+++ src/app/news-single/news-single.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../news.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-news-single',
@@ -7,13 +8,33 @@ import { NewsService } from '../news.service';
   styleUrls: ['./news-single.page.scss'],
 })
 export class NewsSinglePage implements OnInit {
+  text = 'Check out this story!';
   article : any;
   constructor(
-    private newsService :NewsService
+    private newsService :NewsService,
+    private socialSharing: SocialSharing
     ) { }
 
   ngOnInit() {
     this.article = this.newsService.currentArticle;
+  }
+
+  async shareTwitter() {
+    // Either URL or Image
+    this.socialSharing.shareViaTwitter(null, null, this.article.url).then(() => {
+      // Success
+    }).catch((e) => {
+      // Error!
+    });
+  }
+ 
+  async shareWhatsApp() {
+    // Text + Image or URL works
+    this.socialSharing.shareViaWhatsApp(this.text, null, this.article.url).then(() => {
+      // Success
+    }).catch((e) => {
+      // Error!
+    });
   }
 
 }

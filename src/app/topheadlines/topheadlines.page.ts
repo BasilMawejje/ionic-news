c85b35388e0909895fc1be3451749c28759cd700
BasilMawejje <basil.mawejje@gmail.com>
2019-02-22 17:51:51 +0300

    Add pull to refresh Add toast that can be closed after refreshing top headlines

--- src/app/topheadlines/topheadlines.page.ts
+++ src/app/topheadlines/topheadlines.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../news.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-topheadlines',
@@ -9,7 +10,9 @@ import { Router } from '@angular/router';
 })
 export class TopheadlinesPage implements OnInit {
   data : any;
-  constructor(private newsService: NewsService, private router: Router) { }
+  constructor(private newsService: NewsService, 
+              private router: Router,
+              public toastController: ToastController) { }
 
   ngOnInit() {
     this.newsService
@@ -19,6 +22,30 @@ export class TopheadlinesPage implements OnInit {
           this.data = data;
         });
 
+  }
+
+  doRefresh(event) {
+    this.newsService
+        .getData('top-headlines?country=us&category=business')
+        .subscribe(data => {
+          console.log(data);
+          this.data = data;
+        });
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+    this.presentToast();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Fetching new data',
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'Done',
+      color: 'dark'
+    });
+    toast.present();
   }
 
   onGoToNewsSinglePage(article){
